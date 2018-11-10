ee365b06d2800c023d684d93026361d014a6c1bf
mohammad <khezri@outlook.com>
2018-11-10 11:39:45 -0600

    run dada from firebase

--- src/helpers/dataGetter.js
+++ src/helpers/dataGetter.js
@@ -5,7 +5,6 @@ import apiKeys from '../../db/apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getAllPlayersFromDb = () => new Promise((resolve, reject) => {
-
   axios.get(`${baseUrl}/players.json`)
     .then((result) => {
       const allPlayersObject = result.data;
@@ -17,7 +16,6 @@ const getAllPlayersFromDb = () => new Promise((resolve, reject) => {
           allplayersArray.push(newPlayer);
         });
       }
-      console.log('array?', allplayersArray);
 
       resolve(allplayersArray);
     })
@@ -39,15 +37,51 @@ const getPlayersByTeam = teamId => new Promise((resolve, reject) => {
     });
 });
 
-const getAllTeamsFromDb = () => axios.get(`${baseUrl}/teams.json`);
+const getAllTeamsFromDb = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/teams.json`)
+    .then((result) => {
+      const allPlayersObject = result.data;
+      const allplayersArray = [];
+      if (allPlayersObject != null) {
+        Object.keys(allPlayersObject).forEach((playerId) => {
+          const newPlayer = allPlayersObject[playerId];
+          newPlayer.id = playerId;
+          allplayersArray.push(newPlayer);
+        });
+      }
 
-const getAllPositionsFromDb = () => axios.get(`${baseUrl}/positions.json`);
+      resolve(allplayersArray);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+const getAllPositionsFromDb = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/positions.json`)
+    .then((result) => {
+      const allPlayersObject = result.data;
+      const allplayersArray = [];
+      if (allPlayersObject != null) {
+        Object.keys(allPlayersObject).forEach((playerId) => {
+          const newPlayer = allPlayersObject[playerId];
+          newPlayer.id = playerId;
+          allplayersArray.push(newPlayer);
+        });
+      }
+
+      resolve(allplayersArray);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
 const getFullPlayerInfo = players => Promise.all([getAllTeamsFromDb(), getAllPositionsFromDb()])
   .then((dataArray) => {
     const playersFromDb = players;
-    const teamsFromDb = dataArray[0].data;
-    const positionsFromDb = dataArray[1].data;
+    const teamsFromDb = dataArray[0];
+    const positionsFromDb = dataArray[1];
     const newPlayers = [];
     playersFromDb.forEach((player) => {
       const newPlayer = player;
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.scss';
 const getAndPrintTeamButtonGroup = () => {
   dataGetter.getAllTeamsFromDb()
     .then((data) => {
-      $('#button-container').html(createTeamButtonGroup(data.data));
+      $('#button-container').html(createTeamButtonGroup(data));
       $('.team-button').on('click', teamButton.buttonEventFunction);
     })
     .catch((error) => {
@@ -24,7 +24,7 @@ const getAndPrintTeamButtonGroup = () => {
 const getAndPrintAllPlayers = () => {
   dataGetter.getAllPlayersFromDb()
     .then((players) => {
-      dataGetter.getFullPlayerInfo(players.data)
+      dataGetter.getFullPlayerInfo(players)
         .then((allPlayersArray) => {
           $('#main-container').html(createPlayerList(allPlayersArray));
         });
